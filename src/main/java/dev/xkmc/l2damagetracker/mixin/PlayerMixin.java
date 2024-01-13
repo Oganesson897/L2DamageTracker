4568a834020eabd98b3c2d0cc48f13abf6b62b0b
lcy0x1 <arthurlcy0x1@gmail.com>
2024-01-13 18:12:19 -0500

    fix crit visuals

--- src/main/java/dev/xkmc/l2damagetracker/mixin/PlayerMixin.java
+++ src/main/java/dev/xkmc/l2damagetracker/mixin/PlayerMixin.java
@@ -2,8 +2,10 @@ package dev.xkmc.l2damagetracker.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.player.CriticalHitEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,6 +17,11 @@ public class PlayerMixin {
 	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraftforge/event/entity/player/CriticalHitEvent;getDamageModifier()F", remap = false), method = "attack")
 	public void l2damagetracker$attack$allowSweep(Entity target, CallbackInfo ci, @Local(ordinal = 2) LocalBooleanRef flag2) {
 		flag2.set(false);
+	}
+
+	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;getFireAspect(Lnet/minecraft/world/entity/LivingEntity;)I"), method = "attack")
+	public void l2damagetracker$attack$critParticle(Entity target, CallbackInfo ci, @Local(ordinal = 2) LocalBooleanRef flag2, @Local LocalRef<CriticalHitEvent> hitResult) {
+		flag2.set(hitResult.get() != null);
 	}
 
 }
