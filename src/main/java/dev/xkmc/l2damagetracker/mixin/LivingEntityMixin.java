b9522282f022117a7c8d4fd0ca9189727c1634d7
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-06-01 13:59:46 -0400

    absorption fix

--- src/main/java/dev/xkmc/l2damagetracker/mixin/LivingEntityMixin.java
+++ src/main/java/dev/xkmc/l2damagetracker/mixin/LivingEntityMixin.java
@@ -1,9 +1,12 @@
 package dev.xkmc.l2damagetracker.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.xkmc.l2damagetracker.contents.curios.TotemHelper;
 import dev.xkmc.l2serial.util.Wrappers;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.common.ForgeHooks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,7 +19,18 @@ public abstract class LivingEntityMixin {
 	public void l2damagetracker$checkTotemDeathProtection$addCustomTotem(DamageSource pDamageSource, CallbackInfoReturnable<Boolean> cir) {
 		LivingEntity self = Wrappers.cast(this);
 		if (TotemHelper.process(self, pDamageSource)) cir.setReturnValue(true);
+	}
 
+	@WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getDamageAfterMagicAbsorb(Lnet/minecraft/world/damagesource/DamageSource;F)F"), method = "actuallyHurt")
+	public float l2damagetracker$actuallyHurt$moveLivingDamagePre(LivingEntity self, DamageSource source, float damage, Operation<Float> original) {
+		float ans = original.call(self, source, damage);
+		return ForgeHooks.onLivingDamage(self, source, ans);
+	}
+
+
+	@WrapOperation(at = @At(value = "INVOKE", remap = false, target = "Lnet/minecraftforge/common/ForgeHooks;onLivingDamage(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/damagesource/DamageSource;F)F"), method = "actuallyHurt")
+	public float l2damagetracker$actuallyHurt$moveLivingDamagePost(LivingEntity self, DamageSource source, float damage, Operation<Float> original) {
+		return damage;
 	}
 
 }
--- src/main/java/dev/xkmc/l2damagetracker/mixin/PlayerMixin.java
+++ src/main/java/dev/xkmc/l2damagetracker/mixin/PlayerMixin.java
@@ -1,10 +1,15 @@
 package dev.xkmc.l2damagetracker.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.event.entity.player.CriticalHitEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,6 +27,19 @@ public class PlayerMixin {
 	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;getFireAspect(Lnet/minecraft/world/entity/LivingEntity;)I"), method = "attack")
 	public void l2damagetracker$attack$critParticle(Entity target, CallbackInfo ci, @Local(ordinal = 2) LocalBooleanRef flag2, @Local LocalRef<CriticalHitEvent> hitResult) {
 		flag2.set(hitResult.get() != null);
+	}
+
+
+	@WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;getDamageAfterMagicAbsorb(Lnet/minecraft/world/damagesource/DamageSource;F)F"), method = "actuallyHurt")
+	public float l2damagetracker$actuallyHurt$moveLivingDamagePre(Player self, DamageSource source, float damage, Operation<Float> original) {
+		float ans = original.call(self, source, damage);
+		return ForgeHooks.onLivingDamage(self, source, ans);
+	}
+
+
+	@WrapOperation(at = @At(value = "INVOKE", remap = false, target = "Lnet/minecraftforge/common/ForgeHooks;onLivingDamage(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/damagesource/DamageSource;F)F"), method = "actuallyHurt")
+	public float l2damagetracker$actuallyHurt$moveLivingDamagePost(LivingEntity entity, DamageSource src, float damage, Operation<Float> original) {
+		return damage;
 	}
 
 }
